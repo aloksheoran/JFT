ee25095a7c1c4e7ba6db25b039f42e5b45329372
VBONTHA <vbontha@gmail.com>
2018-08-30 22:36:21 -0400

    adding values

---
+++ edureka2508/src/day2/VariableExample.java
@@ -0,0 +1,26 @@
+package day2;
+
+public class VariableExample {
+
+	public static void main(String[] args) {
+		int firstnumber;
+		firstnumber=23;
+		int SecondNumber = 78;
+		int sum = firstnumber + SecondNumber;
+		System.out.println("sum :"+ sum);
+		
+		
+		
+		
+		
+		System.out.println("First Number :"+ firstnumber);
+		
+		System.out.println("First Number :"+firstnumber+ SecondNumber);
+		System .out .println("Sum of two numbers number :"+ (firstnumber+SecondNumber));
+		
+		byte myFirstByteNum = 70;
+		 
+		System. out.println("Byte number : "+ myFirstByteNum);
+	}
+
+}
