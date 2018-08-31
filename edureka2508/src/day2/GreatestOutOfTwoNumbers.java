52d1fb27f2507ddf1a63041e8a7f87b4e6983d0c
VBONTHA <vbontha@gmail.com>
2018-08-31 12:19:27 -0400

    Adding greatest out of two numbers

---
+++ edureka2508/src/day2/GreatestOutOfTwoNumbers.java
@@ -0,0 +1,19 @@
+package day2;
+
+public class GreatestOutOfTwoNumbers {
+
+	public static void main(String[] args) {
+		int firstNum, secondNum;
+		firstNum = 90;
+		secondNum = 90;	
+		if(firstNum>secondNum){ 
+			System.out.println("First Number is greatest");
+		} 
+		if(firstNum == secondNum) {
+			System.out.println("Both are equal");
+		} else {
+			System.out.println("Second Number is greatest");
+		}
+	}
+
+	}
