27aed059c54864060a382339f50841a3923c3141
Alok <aloksheoran@gmail.com>
2019-01-06 17:09:03 +0530

    test

---
+++ javafortesters/src/test/java/javafortesters/javafortesters/IntegerExamplesTest.java
@@ -0,0 +1,23 @@
+package javafortesters.javafortesters;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IntegerExamplesTest {
+
+    @Test
+   public void integerExploration () {
+
+        Integer four = new Integer(4);
+        Assert.assertEquals("int value returns int 4",4,four.intValue());
+
+    }
+
+    @Test
+    public void hexExplore() {
+
+        Integer s = new Integer(11);
+        Assert.assertEquals("11 becomes b", "b",s.toHexString(11));
+    }
+
+}
--- javafortesters/src/test/java/javafortesters/javafortesters/MyFirstTest.java
+++ javafortesters/src/test/java/javafortesters/javafortesters/MyFirstTest.java
@@ -1,10 +1,15 @@
 package javafortesters.javafortesters;
 
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
 
 public class MyFirstTest {
-	
-	@Test
-	
+
+    @Test
+    public void canAddTwoPlusTwo () {
+     int answer = 2-2;
+     assertEquals("2+2=4",0, answer);
+    }
 
 }
