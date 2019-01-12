a0343fcd48d0ded66b2f03391079ec164844d1a5
Alok <aloksheoran@gmail.com>
2019-01-12 10:19:59 +0530

    test

--- javafortesters/src/main/java/com/javafortesters/domainentities/User.java
+++ javafortesters/src/main/java/com/javafortesters/domainentities/User.java
@@ -2,5 +2,25 @@ package com.javafortesters.domainentities;
 
 
 public class User {
+	
+	public String username; 
+	public String password; 
+	
+	public User(){ 
+		username = "username"; 
+		password = "password"; 
+		
+	}
+
+
+	public Object getUsername() {
+		// TODO Auto-generated method stub
+		return username;
+	}
+
+	public Object getPassword() {
+		// TODO Auto-generated method stub
+		return password;
+	}
 
 }
--- javafortesters/src/test/java/com/javafortesters/chap006domainentities/examples/UserTest.java
+++ javafortesters/src/test/java/com/javafortesters/chap006domainentities/examples/UserTest.java
@@ -1,7 +1,29 @@
 package com.javafortesters.chap006domainentities.examples;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.javafortesters.domainentities.User;
 
 
 
 public class UserTest {
+	
+	@Test 
+	
+	 public void userHasDefaultUsernameAndPassword(){
 
+		User user = new User();
+		
+		user.username = "bob";
+
+
+		assertEquals(" default username expected", "bob", user.getUsername()); 
+		
+		assertEquals(" default password expected", "password", user.getPassword());
+
+		
+		
+     }
+	
 }
